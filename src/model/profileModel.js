d36edfc31d83860cae05f2fee495a8624d6f565f
gusttavodeodato <guustavodeodato@outlook.com>
2026-06-22 19:14:10 -0300

    feat: adiciona profileModel como modelagem de banco de dados

---
+++ src/model/profileModel.js
@@ -0,0 +1,18 @@
+import  { EntitySchema } from "typeorm";
+
+const profile = new EntitySchema({
+    name: "profile",
+    tableName: "tb_profiles",
+    columns: {
+        id: {primary: true, type: "int", generated: true},
+        url_photo_profile: {type: "varchar", length: 250, nullable: false},
+    },
+    relations: {
+        user: {
+            type: "many-to-one", 
+            target: "user", nullable: false
+        },
+    },
+});
+
+export default profile;
