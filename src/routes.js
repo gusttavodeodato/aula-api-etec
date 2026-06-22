5a877f26623e0c895a92416cdf969841e6e344e7
gusttavodeodato <guustavodeodato@outlook.com>
2026-06-22 19:15:35 -0300

    feat: adiciona nova rota para upload com authenticate

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import directorController from "./controllers/directorController.js";
 import generoController from "./controllers/generoController.js";
 import premiacaoController from "./controllers/premiacaoController.js";
 import loginController from "./controllers/loginController.js";
+import uploadController from "./controllers/uploadController.js";
 import { authenticate } from "./utils/jwt.js";
 
 // variavel routes instanciando express
@@ -15,6 +16,7 @@ routes.use("/actor", authenticate, actorController);
 routes.use("/director", authenticate, directorController);
 routes.use("/genero", authenticate, generoController);
 routes.use("/premiacao", authenticate, premiacaoController);
+routes.use("/upload", authenticate, uploadController);
 routes.use("/login", loginController);
 
 export default routes;
